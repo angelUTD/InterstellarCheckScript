2a436eb6d70466d05b4caae3b653a5e834d6cd2e
Angel Jr <72530143+angelUTD@users.noreply.github.com>
2024-07-08 21:03:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import checkAndEmail
 import logger
-
+#Just the driver code
 
 def main():
     #Checking to see if interstellar is in theaters
+    #sotre the result for the logger
     statusOfMovie = checkAndEmail.webScraper()
     if statusOfMovie == True:
         checkAndEmail.sendEmail()

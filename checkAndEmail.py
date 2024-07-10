c626bd8ef3ee7ccd9da88ad78763e9dfe32712c4
Angel Jr <72530143+angelUTD@users.noreply.github.com>
2024-07-10 13:46:17 -0500

    Update checkAndEmail.py

--- checkAndEmail.py
+++ checkAndEmail.py
@@ -2,10 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 import smtplib
 import os
-from dotenv import load_dotenv, dotenv_values 
+
 
 
 def  webScraper():
+    
     url = 'https://www.cinemark.com/movies/coming-soon'
     url2 = 'https://www.cinemark.com/movies/events'
     movie = "Instellar"
@@ -15,14 +16,14 @@ def  webScraper():
     soup = BeautifulSoup(comingSoonPage.text, "html.parser")
     quotesComingSoon = soup.find_all('div', class_='title')
     quotesEvent = soup.find_all('div', class_='title')
-    
+
     for c in quotesComingSoon:
         if c.contents[0] == f"{movie}" or c.contents[0] == f"{movie2}":
             return True
             print("Tickets are out fool get them asap!!!!!")
 
     for e in quotesEvent:
-        if e.contonts[0] == f"{movie}" or e.contents[0] == f"{movie2}":
+        if e.contents[0] == f"{movie}" or e.contents[0] == f"{movie2}":
             return True
 
 
@@ -30,7 +31,9 @@ def sendEmail():
     dotenv_path = os.path.join(os.path.dirname(__file__), 'resources', '.env')
     load_dotenv(dotenv_path)
     key = os.getenv("CINEMA_PASSKEY")
+
     email = os.getenv("EMAIL")
+    emailH = os.getenv("H_MAIL")
 
     subject = "The Movie Lords has blessed you with goodnews"
     message = "Interstellar Tickets are now on sale so go get that done! Heres the link :) \n https://www.cinemark.com/theatres/tx-dallas/cinemark-dallas-xd-and-imax"
@@ -42,4 +45,5 @@ def sendEmail():
 
     server.login(email, key)
     server.sendmail(email, email, text)
+    server.sendmail(email, emailH, text)
     print("Email has been sent for this test")

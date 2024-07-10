92e04577fedaecde8d2c703718ef4ec6513abeee
Angel Jr <72530143+angelUTD@users.noreply.github.com>
2024-07-10 13:46:54 -0500

    Update logger.py

--- logger.py
+++ logger.py
@@ -3,13 +3,12 @@ from datetime import datetime
 #also the time the script was executed to make sure its running on time.
 
 
-
 def logIt(status):
-    logFile = open('resources/log', 'a')
-    messageSuccess = 'Interstellar is finally availbe for advance tickets!!!!!!!!!!!'
-    messageFail = 'Interstellar was Not found at Cinemarks website'
-    current_time =  datetime.now()
-    if(status):
-        logFile.write(f"{current_time} - {messageSuccess} -\n")
-    else:
-        logFile.write(f"{current_time} - {messageFail} -\n")
+	logFile = open('/home/heavenly/Documents/InterStellarScript/InterstellarCheckScript-main/resources/log', 'a')
+	messageSuccess = 'Interstellar is finally availbe for advance tickets!!!!!!!!!!!'
+	messageFail = 'Interstellar was Not found at Cinemarks website'
+	current_time =  datetime.now()
+	if(status):
+		logFile.write(f"{current_time} - {messageSuccess} -\n")
+	else:
+		logFile.write(f"{current_time} - {messageFail} -\n")

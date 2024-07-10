b66750d7f17bc69e34f0837dd49d713b94f96e37
Angel Jr <72530143+angelUTD@users.noreply.github.com>
2024-07-10 13:44:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-import checkAndEmail
+
+import StarSearch
 import logger
 #Just the driver code
 
 def main():
-    #Checking to see if interstellar is in theaters
-    #sotre the result for the logger
-    statusOfMovie = checkAndEmail.webScraper()
-    if statusOfMovie == True:
-        checkAndEmail.sendEmail()
-    logger.logIt(statusOfMovie)  
+	#Checking to see if interstellar is in theaters
+	#sotre the result for the logger
+
+	statusOfMovie = StarSearch.webScraper()
+	if statusOfMovie == True:
+		StarSearch.sendEmail() 
+		statusOfMovie = True
+		logger.logIt(statusOfMovie)
+	else:
+		statusOfMovie = False
+		logger.logIt(statusOfMovie)
+		
 
 
-    
 
 if __name__ == "__main__":
     main()
-
 
